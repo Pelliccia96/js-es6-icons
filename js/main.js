644b931ab2bf87a2cce7f260482478db22110b17
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-11-18 17:46:51 +0100

    Adds Main Cards (Milestone 1 + 2)

---
+++ js/main.js
@@ -0,0 +1,142 @@
+const iconsArray = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+const maxConainer = document.getElementById("max-container");
+const cardContainer = document.querySelector("#max-container .row");
+
+iconsArray.forEach((element, i) => {
+    console.log(element)
+
+    const cardCol = createElement("div", "col", "p-3");
+    const card = createElement("div", "card", "text-center");
+    const iconClass1 = element.prefix + element.name
+    console.log(element.prefix+element.name)
+
+    const iconClass2 = element.prefix + "solid"
+    const cardIcon = createElement("i", iconClass1, iconClass2);
+    cardIcon.style.color = element.color
+    const cardTitle = createElement("h5", "title", "text-uppercase");
+    cardTitle.innerHTML = element.name
+    cardCol.append(card);
+    card.append(cardIcon, cardTitle);
+    cardContainer.append(cardCol);
+});
+
+function createElement(tagEl, class1, class2) {
+    const created_El = document.createElement(tagEl);
+    created_El.classList.add(class1);
+    created_El.classList.add(class2);
+    return created_El
+};
